2a97c2ccf9097211551eb019fb9fc06fa06fb130
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-08-03 16:43:29 +0700

    update view register

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -23,39 +23,50 @@ class SocialiteController extends Controller
         } catch (Exception $e) {
             return redirect()->back();
         }
-
+        // find or create user and send params user get from socialite and provider
         $authUser = $this->findOrCreateUser($user, $provider);
 
+        // login user
         Auth()->login($authUser, true);
 
+        // setelah login redirect ke dashboard
         return redirect()->route('dashboard');
     }
 
     public function findOrCreateUser($socialUser, $provider)
     {
+        // Get Social Account
         $socialAccount = SocialAccount::where('provider_id', $socialUser->getId())
             ->where('provider_name', $provider)
             ->first();
 
+        // Jika sudah ada
         if ($socialAccount) {
-
+            // return user
             return $socialAccount->user;
+
+            // Jika belum ada
         } else {
 
+            // User berdasarkan email 
             $user = User::where('email', $socialUser->getEmail())->first();
 
+            // Jika Tidak ada user
             if (!$user) {
+                // Create user baru
                 $user = User::create([
                     'name'  => $socialUser->getName(),
                     'email' => $socialUser->getEmail()
                 ]);
             }
 
+            // Buat Social Account baru
             $user->socialAccounts()->create([
                 'provider_id'   => $socialUser->getId(),
                 'provider_name' => $provider
             ]);
 
+            // return user
             return $user;
         }
     }
